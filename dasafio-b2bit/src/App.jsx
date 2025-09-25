f2cc2a8d7fc7fa75517fa71b724d502ef727f89a
Diego Carvalho <diegoricardo2527@gmail.com>
2025-09-24 23:26:45 -0300

    feat: criar página Dashboard com proteção de rota e logout

--- dasafio-b2bit/src/App.jsx
+++ dasafio-b2bit/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Bem-vindo ao desafio B2bit 🚀</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
