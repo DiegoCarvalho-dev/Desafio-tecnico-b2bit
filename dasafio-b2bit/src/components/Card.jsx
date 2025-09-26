a785868085300847f14a4ff9079b15fdd360b74c
Diego Carvalho <diegoricardo2527@gmail.com>
2025-09-26 18:55:28 -0300

    feat(card): criar componente de Card para KPIs

---
+++ dasafio-b2bit/src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Card({ title, value }) {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>{value}</p>
+    </div>
+  );
+}
