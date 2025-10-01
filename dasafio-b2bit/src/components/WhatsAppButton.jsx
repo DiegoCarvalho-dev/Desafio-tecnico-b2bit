4fd9b17879964ceaf7f52d831446b6d9ceb59ad0
Diego Carvalho <diegoricardo2527@gmail.com>
2025-09-30 22:32:57 -0300

    feat: implementar páginas de suporte com notificações e recursos de contato

---
+++ dasafio-b2bit/src/components/WhatsAppButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/5584994182380"
+      className="whatsapp-float"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      💬
+    </a>
+  );
+}
