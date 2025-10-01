cf608a71f9530ed468bb90d3d2b17cf5eb398ab0
Diego Carvalho <diegoricardo2527@gmail.com>
2025-09-30 22:34:44 -0300

    chore: configurar servidor backend com middlewares e configurações de CORS

--- dasafio-b2bit/backend/server.js
+++ dasafio-b2bit/backend/server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
+const productsRoutes = require("./routes/products");
 const dashboardRoutes = require("./routes/dashboard");
-const productRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth"); 
 
 const app = express();
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/products", productsRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
-const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`🚀 Backend rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
