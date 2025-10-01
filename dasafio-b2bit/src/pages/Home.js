f0b061f9514772d50710db68405632deebefd938
Diego Carvalho <diegoricardo2527@gmail.com>
2025-09-30 22:35:28 -0300

    feat: criar página inicial com apresentação e navegação principal

--- dasafio-b2bit/src/pages/Home.js
+++ dasafio-b2bit/src/pages/Home.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "../components/Carousel";
+import WhatsAppButton from "../components/WhatsAppButton";
+import Footer from "../components/Footer";
+import { getProducts } from "../services/api";
 
-function Home() {
+export default function Home() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function carregarProdutos() {
+      try {
+        const res = await getProducts();
+        setProdutos(res);
+      } catch (err) {
+        console.error("Erro ao carregar produtos:", err);
+      }
+    }
+    carregarProdutos();
+  }, []);
+
   return (
     <div>
-      <Carousel />
-      <h2>Bem-vindo à nossa Loja Virtual</h2>
-      <p>Confira nossas novidades e promoções!</p>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Nossos Produtos</h1>
+      <Carousel items={produtos} />
+      <WhatsAppButton />
+      <Footer />
     </div>
   );
 }
-
-export default Home;
